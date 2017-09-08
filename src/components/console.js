571f3e41e38a2f003ab39ef18dc2791187701503
Zhiqing Qiu <zhqqi@microsoft.com>
2017-09-08 12:41:52 +0800

    Fix console height

--- src/components/console.js
+++ src/components/console.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Console from 'react-console-component';
 import 'react-console-component/main.css';
 
@@ -7,6 +8,12 @@ class MyConsole extends Component {
     componentDidUpdate(prevProps, prevState) {
         this.writeLine(this.props.message.substring(prevProps.message.length));
         this.writeLine(this.props.error);
+    }
+
+    componentDidMount() {
+        window.mj = ReactDOM.findDOMNode(this.myConsole)
+        let height = ReactDOM.findDOMNode(this.myConsole).getBoundingClientRect().height;
+        ReactDOM.findDOMNode(this.myConsole).style.height = height + "px";
     }
 
     writeLine = (msg) => {
